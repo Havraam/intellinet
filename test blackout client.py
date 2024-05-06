1d5b58d1df86b17738b9eba413589d87202e607e
Erel <100906261+Havraam@users.noreply.github.com>
2024-05-06 12:47:13 +0300

    whole server framework changed and added documentation

--- test blackout client.py
+++ test blackout client.py
@@ -1,7 +1,7 @@
 from socket import socket
 
 sock = socket()
-sock.connect(("10.30.57.179",5001))
+sock.connect(("192.168.68.103",5001))
 while True:
     message = input("This is where you type in your input request: ")
     message = message.encode()
--- test blackout server.py
+++ test blackout server.py
@@ -1,54 +1,46 @@
-import tkinter as tk
-from socket import socket
-from threading import * 
 import ctypes
 from ctypes import wintypes
+from socket import socket
+from threading import *
+import tkinter as tk
+import pywinauto
 
 
-def disable_screen():
-    pass
+#setting up the blockinput variable
+BlockInput = ctypes.windll.user32.BlockInput
 
-def start_blackout(BlockInput):
-    global root
-    root = tk.Tk()
-    root.attributes("-fullscreen", True, "-topmost", True)
-    root.protocol("WM_DELETE_WINDOW", disable_screen)
-    root.mainloop()
-    blocked = BlockInput(True)
-    if blocked:
-        pass
-    else:
-        raise RuntimeError('input is already blocked by another thread')
-
-def listen_for_requests():
-
-    global root    
-    sock = socket()
-    sock.bind(("10.30.56.202", 5001))
-    try:
-        sock.listen(5)
-        print('Server started.')
-
-        while 'connected':
-            conn, addr = sock.accept()
-            print('Client connected IP:', addr)
-            while(True):
-                request = conn.recv(1024).decode()
-                if request == "BEBlackout":
-                    BlockInput = ctypes.windll.user32.BlockInput
-                    BlackoutThread = Thread(target = start_blackout, args= BlockInput)
-                    BlackoutThread.start()
-                elif request == "STBlackout" :
-                    root.destroy()
-                    unblocked = BlockInput(False)
-                    BlackoutThread.join()
-
-
-    finally:
-        sock.close()
-
-listen_for_requests()
-
-
-
-print("hello")
+#starting up the server
+sock = socket()
+sock.bind(("192.168.68.103",5001))
+sock.listen(5)
+print('Server started.')
+while 'connected':
+    #client acceptance
+    conn,addr = sock.accept()
+    print("client connected IP:",addr)
+    while (True):
+        #endless loop to always recieve requests 
+        request = conn.recv(1024).decode()
+        #if request == BEB - begin blackout than --->
+        if request == "BEB":
+            #stop all currently playing media
+            hllDll = ctypes.WinDLL("User32.dll")
+            VK_STOPMEDIA = 0xB2
+            hllDll.keybd_event(VK_STOPMEDIA,0,1,0)
+            hllDll.keybd_event(VK_STOPMEDIA,0,2,0)
+            #press winkey+d to minimize to desktop
+            pywinauto.keyboard.send_keys("{VK_LWIN down}d{VK_LWIN up}")
+            #completley block all input until told otherwise
+            blocked = BlockInput(True)
+            if blocked:
+                blackout = True
+                try:
+                    #while loop that runs as long as STB - stop blackout request hasnt been recieved 
+                    while(blackout):
+                        dis_request = conn.recv(1024).decode()
+                        if (dis_request == "STB"):
+                            blackout = False
+                finally:
+                    unblocked = BlockInput(False) # unblock when STB request recieved 
+            else:
+                raise RuntimeError('Input is already blocked by another thread!')
